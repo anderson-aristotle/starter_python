aae67b5d87d3d38a04ec474ab6c49db4586a9f53
rex@billion <anderson.aristotle@gmail.com>
2019-09-11 17:34:10 -0400

    added panda, numpy import

---
+++ user_application.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+from datetime import date
+
+interest_rate = 0.04
+years = 30
+payments_year = 12
+addl_principle = 50
+start_date = (date(2016,1,1))
+
+
+
+# smallest_so_far = -1
+# for the_num in [9, 41, 12, 3, 74, 15] :
+#    if the_num < smallest_so_far :
+#       smallest_so_far = the_num
+# print(smallest_so_far)
