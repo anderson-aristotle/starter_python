4b958988619542308007c8dc47fc00fbf3381a89
rex@billion <anderson.aristotle@gmail.com>
2019-09-12 22:18:23 -0400

    financial analysis algoithum

--- user_application.py
+++ user_application.py
@@ -8,7 +8,23 @@ payments_year = 12
 addl_principle = 50
 start_date = (date(2016,1,1))
 
+# Assign `Adj Close` to `daily_close`
+daily_close = aapl[['___________']]
 
+# Daily returns
+daily_pct_change = daily_close.__________()
+
+# Replace NA values with 0
+daily_pct_change.fillna(0, inplace=True)
+
+# Inspect daily returns
+print(______________)
+
+# Daily log returns
+daily_log_returns = np.log(daily_close.pct_change()+1)
+
+# Print daily log returns
+print(daily_log_returns)
 
 # smallest_so_far = -1
 # for the_num in [9, 41, 12, 3, 74, 15] :
