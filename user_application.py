6a50fdf23ea8dd9d9ecc4ed51dd5151efb2e3038
rex@billion <anderson.aristotle@gmail.com>
2019-09-16 11:51:32 -0400

    left else statement

--- user_application.py
+++ user_application.py
@@ -188,3 +188,39 @@ def getLeftMost(root):
     while root.getleft() is not None:
         root = root.getleft()
     return root.getdata()
+    def del_node(root,data):
+        if root.getdata() == data:
+        if root.getleft() is not None and root.getright() is not None:
+            temp_data = getLeftMost(root.getright())
+            root.setdata(temp_data)
+            root.setright(del_node(root.getright(),temp_data))
+        elif root.getleft() is not None:
+            root = root.getleft()
+        else:
+            root = root.getright()
+    elif root.getdata() > data:
+        if root.getleft() is None:
+            print("No such data")
+            return root
+        else:
+            root.setleft(del_node(root.getleft(),data))
+    elif root.getdata() < data:
+        if root.getright() is None:
+            return root
+        else:
+            root.setright(del_node(root.getright(),data))
+            if root is None:
+            return root
+    if getheight(root.getright()) - getheight(root.getleft()) == 2:
+        if getheight(root.getright().getright()) > getheight(root.getright().getleft()):
+            root = rightrotation(root)
+        else:
+            root = lrrotation(root)
+    elif getheight(root.getright()) - getheight(root.getleft()) == -2:
+        if getheight(root.getleft().getleft()) > getheight(root.getleft().getright()):
+            root = leftrotation(root)
+        else:
+            root = rlrotation(root)
+    height = my_max(getheight(root.getright()),getheight(root.getleft())) + 1
+    root.setheight(height)
+    return root
