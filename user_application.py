dff821a483ec75547b90a39f732d1cf2c8026888
rex@billion <anderson.aristotle@gmail.com>
2019-09-16 11:43:15 -0400

    set data structures

--- user_application.py
+++ user_application.py
@@ -43,3 +43,66 @@ config.read('oanda.cfg')  # 4
 
 oanda = opy.API(environment='practice',
                 access_token=config['oanda']['access_token'])  # 5
+# -*- coding: utf-8 -*-
+'''
+An auto-balanced binary tree!
+'''
+import math
+import random
+class my_queue:
+    def __init__(self):
+        self.data = []
+        self.head = 0
+        self.tail = 0
+    def isEmpty(self):
+        return self.head == self.tail
+    def push(self,data):
+        self.data.append(data)
+        self.tail = self.tail + 1
+    def pop(self):
+        ret = self.data[self.head]
+        self.head = self.head + 1
+        return ret
+    def count(self):
+        return self.tail - self.head
+    def print(self):
+        print(self.data)
+        print("**************")
+        print(self.data[self.head:self.tail])
+        
+class my_node:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+        self.height = 1
+    def getdata(self):
+        return self.data
+    def getleft(self):
+        return self.left
+    def getright(self):
+        return self.right
+    def getheight(self):
+        return self.height
+    def setdata(self,data):
+        self.data = data
+        return
+    def setleft(self,node):
+        self.left = node
+        return
+    def setright(self,node):
+        self.right = node
+        return
+    def setheight(self,height):
+        self.height = height
+        return
+
+def getheight(node):
+    if node is None:
+        return 0
+    return node.getheight()
+
+def my_max(a,b):
+    if a > b:
+        return a
+    return b
